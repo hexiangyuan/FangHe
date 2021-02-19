dba1e3bafe261966c3c91ad95e33b466f17b132f
何祥源 <xiangyuan.he@aihuishou.com>
2021-02-19 18:15:23 +0800

    img size

--- app/screens/find/ptoto-detail-screen/PhotoDetailScreen.tsx
+++ app/screens/find/ptoto-detail-screen/PhotoDetailScreen.tsx
@@ -1,109 +1,72 @@
-import React, {useEffect, useState} from 'react';
-import {Dimensions, FlatList, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Dimensions, FlatList, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import ImageViewer from "react-native-image-zoom-viewer";
-import {SafeAreaView} from "react-native-safe-area-context";
-import {useLocalStore} from "mobx-react-lite";
-import {Icon} from "../../../components";
-import {BottomModal, ModalContent} from 'react-native-modals';
-import {CommentItem} from "../../main-screen/find-tab/components/CommentItem";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { observer, useLocalStore, useObserver } from "mobx-react-lite";
+import { Icon } from "../../../components";
+import { BottomModal, ModalContent } from "react-native-modals";
+import { CommentItem } from "../../main-screen/find-tab/components/CommentItem";
 import FindApi from "../FindApi";
-
-const PhotoDetailScreen = (props) => {
-
+const PhotoDetailScreen = props => {
   const store = useLocalStore(() => ({
     id: 0,
     data: {
       count: 1,
-      images: [{
-        // Simplest usage.
-        url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fg-search1.alicdn.com%2Fimg%2Fbao%2Fuploaded%2Fi4%2F2910563675%2FTB2T8mailjTBKNjSZFDXXbVgVXa_%21%212910563675.png_300x300.jpg&refer=http%3A%2F%2Fg-search1.alicdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613271649&t=794423d2d74da04983ed7b4cde2c826b',
-
-        // width: number
-        // height: number
-        // Optional, if you know the image size, you can set the optimization performance
-
-        // You can pass props to <Image />.
-        props: {
-          // headers: ...
-        }
-      }, {
-        url: '',
-        props: {
-          // Or you can set source directory.
-          source: "require('../../../components/icon/icons/home_empty.png')"
-        }
-      }],
+      isLiked: false,
+      isCollected: false,
+      images: null,
+      likesNum: 0,
+      commentNum: 0,
+      commentList: []
     },
     commentPage: 0,
     refreshing: false,
     get empty() {
       if (store.data) {
-        return store.data.length === 0;
+        return store.data.images.length === 0;
       } else {
         return false;
       }
     },
-    refreshData() {
-      // store.refreshing = true;
-      // props.imgs.forEach((value, index) => store.data.images[index] = {url: value, props: {}})
-      FindApi.getImgsDetail(store.id).then(value => {
-        store.refreshing = false;
-        console.log("response data==== ", value.code);
-        if (value.code === 200) {
-          const imgs = value.data.imgs
-          if (imgs != null && imgs.length > 0) {
-            const list = new Array(imgs.length)
-            imgs.forEach((v, i) => {
-              list[i] = {
-                url: v,
-                props: {}
-              }
-            })
-            setDataList(list)
-          }
-          if (value.data.collected) {
-            setIsCollected(true)
-          }
-          if (value.data.likes) {
-            setIsLiked(true)
-          }
-          setLikesNum(value.data.likesNum)
-          setCommentNum(value.data.commentNum)
-        }
-      });
-
+    cancelLike() {
+      store.data.isLiked = false;
+    },
+    cancelCollect() {
+      store.data.isCollected = false;
     }
   }));
 
-  const [dataList, setDataList] = useState([])
-  const [commentList, setCommentList] = useState([])
-  const [isCollected, setIsCollected] = useState(false)
-  const [isLiked, setIsLiked] = useState(false)
-  const [likesNum, setLikesNum] = useState(0)
-  const [commentNum, setCommentNum] = useState(0)
-  const [commentValue, setCommentValue] = useState("")
+  const [commentValue, setCommentValue] = useState("");
+
+  function getImgDetails(id: number) {
+    store.refreshing = true;
+    FindApi.getImgsDetail(id).then(value => {
+      store.refreshing = false;
+      console.log("response data==== ", value.code);
+      if (value.code === 200) {
+        if (value.data && value.data.imgs) {
+          store.data.images = value.data.imgs;
+        }
+        store.data.isLiked = value.data.likes;
+        store.data.isCollected = value.data.collected;
+        store.data.likesNum = value.data.likesNum;
+        store.data.commentNum = value.data.commentNum;
+        console.log(store.data.images);
+      }
+    });
+  }
 
   /**
    * 点击收藏
    */
   function clickCollect() {
     // TODO 检查登陆
-    if (isCollected) {
-      FindApi.cancelImgsCollect(store.id).then(value => {
-        if (value.code === 200) {
-          setIsCollected((current) => !current)
-        } else {
-
-        }
-      })
+    if (store.data.isCollected) {
+      store.cancelCollect();
+      FindApi.cancelImgsCollect(store.id).then(value => {});
     } else {
-      FindApi.addImgsCollect(store.id).then(value => {
-        if (value.code === 200) {
-          setIsCollected((current) => !current)
-        } else {
-
-        }
-      });
+      store.data.isCollected = true;
+      FindApi.addImgsCollect(store.id).then(value => {});
     }
   }
 
@@ -112,18 +75,14 @@ const PhotoDetailScreen = (props) => {
    */
   function clickLike() {
     // TODO 检查登陆
-    if (isLiked) {
-      FindApi.cancelImgsLike(store.id).then(value => {
-        if (value.code === 200) {
-          setIsLiked((current) => !current)
-        }
-      })
+    if (store.data.isLiked) {
+      store.cancelLike();
+      store.data.likesNum--;
+      FindApi.cancelImgsLike(store.id).then(value => {});
     } else {
-      FindApi.addImgsLike(store.id).then(value => {
-        if (value.code === 200) {
-          setIsLiked((current) => !current)
-        }
-      });
+      store.data.isLiked = true;
+      store.data.likesNum++;
+      FindApi.addImgsLike(store.id).then(value => {});
     }
   }
 
@@ -131,7 +90,7 @@ const PhotoDetailScreen = (props) => {
    * 点击评论，弹出评论列表
    */
   function clickComment() {
-    setCommentListWindowVisible(true)
+    setCommentListWindowVisible(true);
   }
 
   /**
@@ -145,25 +104,21 @@ const PhotoDetailScreen = (props) => {
     }).then(value => {
       if (value.code === 200) {
         // TODO 需要返回当前评论
-        // setCommentList(current => current.concat([commentValue]))
-        setCommentValue("")
-        setAddCommentWindowVisible(false)
+        setAddCommentWindowVisible(false);
       }
     });
   }
 
   function getCommentList(isRefresh) {
-
     FindApi.getImgsCommentList(store.id, isRefresh ? 0 : store.commentPage).then(value => {
-      console.log("response data==== ", value.code);
       if (value.code === 200) {
         if (isRefresh) {
-          setCommentList(value.data)
-          store.commentPage = 1
+          store.data.commentList = value.data;
+          store.commentPage = 1;
         } else {
           if (value.data.length > 0) {
-            setCommentList(current => current.concat(value.data))
-            store.commentPage += 1
+            store.data.commentList = store.data.commentList.concat(value.data);
+            store.commentPage += 1;
           }
         }
       }
@@ -171,135 +126,148 @@ const PhotoDetailScreen = (props) => {
   }
 
   function onLoadMore() {
-    getCommentList(false)
+    getCommentList(false);
   }
 
   useEffect(() => {
-    store.id = props.route.params.id
-    store.refreshData();
-    getCommentList(true)
+    store.id = props.route.params.id;
+    getImgDetails(store.id);
+    getCommentList(true);
   }, []);
 
-  const [commentListWindowVisible, setCommentListWindowVisible] = useState(false)
-  const [addCommentWindowVisible, setAddCommentWindowVisible] = useState(false)
+  const [commentListWindowVisible, setCommentListWindowVisible] = useState(false);
+  const [addCommentWindowVisible, setAddCommentWindowVisible] = useState(false);
 
-  return (
-    <SafeAreaView
-      edges={["top"]}
-      style={{
-        flex: 1,
-        backgroundColor: "white",
-        flexDirection: "row"
-      }}
-    >
-      <View style={styles.main_wrapper}>
+  return useObserver(() => {
+    console.log(store.data.images);
+    return (
+      <SafeAreaView
+        edges={["top"]}
+        style={{
+          flex: 1,
+          backgroundColor: "white",
+          flexDirection: "row"
+        }}
+      >
+        <View style={styles.main_wrapper}>
+          {store.data.images && (
+            <ImageViewer imageUrls={store.data.images?.map(item => ({ url: item }))} backgroundColor={"#333"} />
+          )}
 
-        <ImageViewer imageUrls={store.data.images} backgroundColor={"#333"}/>
+          <View style={styles.bottom_wrapper}>
+            <Pressable style={styles.bottom_button_wrapper} onPress={clickCollect}>
+              <Icon
+                style={styles.bottom_button_image}
+                icon={store.data.isCollected ? "collect_true" : "collect_false"}
+              />
 
-        <View style={styles.bottom_wrapper}>
-          <Pressable style={styles.bottom_button_wrapper}
-                     onPress={clickCollect}>
-            <Icon style={styles.bottom_button_image}
-                  icon={isCollected ? "collect_true" : "collect_false"}/>
+              <Text style={styles.bottom_button_text}>收藏</Text>
+            </Pressable>
+            <Pressable style={styles.bottom_button_wrapper} onPress={clickLike}>
+              <Icon style={styles.bottom_button_image} icon={store.data.isLiked ? "like_true" : "like_false"} />
+              <Text style={styles.bottom_button_text}>{store.data.likesNum}</Text>
+            </Pressable>
+            <Pressable style={styles.bottom_button_wrapper} onPress={clickComment}>
+              <Icon style={styles.bottom_button_image} icon={"comment"} />
+              <Text style={styles.bottom_button_text}>{store.data.commentNum}</Text>
+            </Pressable>
+          </View>
 
-            <Text style={styles.bottom_button_text}>收藏</Text>
-          </Pressable>
-          <Pressable style={styles.bottom_button_wrapper}
-                     onPress={clickLike}>
-            <Icon style={styles.bottom_button_image}
-                  icon={isLiked ? "like_true" : "like_false"}/>
-            <Text style={styles.bottom_button_text}>{likesNum}</Text>
-          </Pressable>
-          <Pressable style={styles.bottom_button_wrapper}
-                     onPress={clickComment}>
-            <Icon style={styles.bottom_button_image}
-                  icon={"comment"}/>
-            <Text style={styles.bottom_button_text}>{commentNum}</Text>
-          </Pressable>
+          <BottomModal
+            visible={commentListWindowVisible}
+            onTouchOutside={() => {
+              setCommentListWindowVisible(false);
+            }}
+            height={0.6}
+            width={1}
+            onSwipeOut={() => setCommentListWindowVisible(false)}
+          >
+            <ModalContent style={styles.commentListWindow}>
+              <View
+                style={{
+                  height: 400,
+                  display: "flex"
+                }}
+              >
+                <Text style={styles.commentCount}>{store.data.commentNum}条评论</Text>
+
+                <FlatList
+                  style={{
+                    flex: 1,
+                    marginVertical: 10
+                  }}
+                  data={store.data.commentList}
+                  numColumns={1}
+                  keyExtractor={(item, index) => index.toString()}
+                  onEndReachedThreshold={0.1}
+                  onEndReached={() => onLoadMore()}
+                  renderItem={({ item }) => {
+                    return (
+                      <TouchableOpacity
+                        style={{ flex: 1 }}
+                        onPress={() => {
+                          console.log(item.content);
+                        }}
+                      >
+                        <CommentItem {...item} />
+                      </TouchableOpacity>
+                    );
+                  }}
+                />
+
+                <Pressable
+                  onPress={() => {
+                    setAddCommentWindowVisible(true);
+                  }}
+                >
+                  <Text style={styles.btnAddComment}>留下你的精彩评论吧</Text>
+                </Pressable>
+              </View>
+            </ModalContent>
+          </BottomModal>
+
+          <BottomModal
+            visible={addCommentWindowVisible}
+            onTouchOutside={() => {
+              setAddCommentWindowVisible(false);
+            }}
+            height={0.2}
+            width={1}
+            onSwipeOut={() => setAddCommentWindowVisible(false)}
+          >
+            <ModalContent style={{ backgroundColor: "#fff" }}>
+              <View style={styles.inputCommentWrapper}>
+                <TextInput
+                  style={styles.inputComment}
+                  value={commentValue}
+                  onChangeText={text => {
+                    setCommentValue(text);
+                  }}
+                  autoFocus={true}
+                  clearButtonMode={"while-editing"}
+                  placeholder={"留下你的精彩评论吧"}
+                  placeholderTextColor={"#c0c0c0"}
+                />
+                <Pressable style={styles.btnCommitCommentWrapper} onPress={commitComment}>
+                  <Icon style={styles.btnCommitComment} icon={"commit"} />
+                </Pressable>
+              </View>
+            </ModalContent>
+          </BottomModal>
         </View>
-
-        <BottomModal
-          visible={commentListWindowVisible}
-          onTouchOutside={() => {
-            setCommentListWindowVisible(false)
-          }}
-          height={0.6}
-          width={1}
-          onSwipeOut={() => setCommentListWindowVisible(false)}
-        >
-          <ModalContent style={styles.commentListWindow}>
-            <View style={{height: 400, display: "flex"}}>
-
-              <Text style={styles.commentCount}>{commentList.length}条评论</Text>
-
-              <FlatList style={{flex: 1, marginVertical: 10}}
-                        data={commentList}
-                        numColumns={1}
-                        keyExtractor={(item, index) => index.toString()}
-                        onEndReachedThreshold={0.1}
-                        onEndReached={() => onLoadMore()}
-                        renderItem={({item}) => {
-                          return (
-                            <TouchableOpacity style={{flex: 1}}
-                                              onPress={() => {
-                                                console.log(item.content)
-                                              }}>
-                              <CommentItem {...item}/>
-                            </TouchableOpacity>
-                          )
-                        }}/>
-
-              <Pressable onPress={() => {
-                setAddCommentWindowVisible(true)
-              }}>
-                <Text style={styles.btnAddComment}>留下你的精彩评论吧</Text>
-              </Pressable>
-
-            </View>
-
-          </ModalContent>
-        </BottomModal>
-
-        <BottomModal
-          visible={addCommentWindowVisible}
-          onTouchOutside={() => {
-            setAddCommentWindowVisible(false)
-          }}
-          height={0.2}
-          width={1}
-          onSwipeOut={() => setAddCommentWindowVisible(false)}
-        >
-          <ModalContent style={{backgroundColor: '#fff'}}>
-            <View style={styles.inputCommentWrapper}>
-              <TextInput style={styles.inputComment} value={commentValue} onChangeText={text => {
-                setCommentValue(text)
-              }}
-                         autoFocus={true}
-                         clearButtonMode={"while-editing"}
-                         placeholder={"留下你的精彩评论吧"}
-                         placeholderTextColor={"#c0c0c0"}/>
-              <Pressable style={styles.btnCommitCommentWrapper}
-                         onPress={commitComment}>
-                <Icon style={styles.btnCommitComment}
-                      icon={"commit"}/>
-              </Pressable>
-            </View>
-          </ModalContent>
-        </BottomModal>
-
-      </View>
-    </SafeAreaView>
-  );
-}
+      </SafeAreaView>
+    );
+  });
+};
 
 const styles = StyleSheet.create({
   main_wrapper: {
-    flex: 1,
+    flex: 1
   },
   bottom_wrapper: {
     position: "absolute",
     bottom: 55,
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
     height: 50,
     display: "flex",
     flexDirection: "row",
@@ -316,7 +284,7 @@ const styles = StyleSheet.create({
   },
   bottom_button_image: {
     width: 22,
-    height: 22,
+    height: 22
   },
   bottom_button_text: {
     width: 70,
@@ -329,22 +297,22 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 46,
     lineHeight: 46,
-    fontWeight: '800',
+    fontWeight: "800"
   },
   headerContainer: {
     paddingVertical: 24,
-    backgroundColor: 'white',
+    backgroundColor: "white"
   },
   commentListWindow: {
     display: "flex",
     flexDirection: "column",
-    backgroundColor: "white",
+    backgroundColor: "white"
   },
   commentCount: {
     height: 30,
     fontSize: 14,
     color: "black",
-    alignSelf: "center",
+    alignSelf: "center"
   },
   btnAddComment: {
     height: 40,
@@ -358,12 +326,12 @@ const styles = StyleSheet.create({
   },
   inputCommentWrapper: {
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "row"
   },
   inputComment: {
     flex: 1,
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderRadius: 50,
     backgroundColor: "#e7e8e9",
     paddingHorizontal: 20,
@@ -379,7 +347,7 @@ const styles = StyleSheet.create({
   },
   btnCommitComment: {
     width: 22,
-    height: 22,
+    height: 22
   }
 });
 
--- app/screens/main-screen/find-tab/components/FindItem.tsx
+++ app/screens/main-screen/find-tab/components/FindItem.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import {FindItemProps} from "./FindItem.props";
-import {Dimensions, StyleSheet, Text, View} from "react-native";
-import FastImage from 'react-native-fast-image'
+import { FindItemProps } from "./FindItem.props";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import FastImage from "react-native-fast-image";
 
-const imageWidth = (Dimensions.get('window').width - 12 * 3) / 2
+const imageWidth = (Dimensions.get("window").width - 12 * 3) / 2;
+const imageHeight = imageWidth * (16 / 9);
 
 const styles = StyleSheet.create({
   imgStyle: {
-    flex: 1,
-    height: 200,
+    height: imageHeight,
     width: imageWidth,
-    backgroundColor: "#610"
+    backgroundColor: "white"
   },
   itemContainer: {
     flex: 1,
     paddingTop: 21,
     paddingHorizontal: 12,
     flexWrap: "wrap",
-    alignItems: "center",
+    alignItems: "center"
   }
 });
 
@@ -30,44 +30,42 @@ const keyTabPhoto = 2;
 const keyTabVideo = 3;
 
 export const FindItem = (props: FindItemProps) => {
-
   function transformLikeNum(num: number): string {
     if (num >= 1000) {
-      return (num / 1000.0).toString() + "k"
+      return (num / 1000.0).toString() + "k";
     }
-    return num.toString()
+    return num.toString();
   }
 
   return (
     <View style={styles.itemContainer}>
       <FastImage
         source={{
-          uri: props.type == keyTabVideo ? props.mainImg : props.imgs[0],
+          uri: props.type == keyTabVideo ? props.mainImg : props.imgs[0]
         }}
-        resizeMode={FastImage.resizeMode.cover}
+        resizeMode={FastImage.resizeMode.center}
         style={styles.imgStyle}
       />
       <Text
+        numberOfLines={1}
         style={{
           fontSize: 16,
+          marginTop: 12,
           lineHeight: 22,
-          color: "#333",
-          flexWrap: "wrap",
-          height: 44,
-        }}>
+          color: "#333"
+        }}
+      >
         {props.title}
       </Text>
       <View
         style={{
           flexDirection: "row",
           flex: 1,
-          marginTop: 5,
-        }}>
-        <Text style={{lineHeight: 17, fontSize: 12, color: "#666", flex: 1}}>
-          评论 {props.commentNum}
-        </Text>
-        <Text
-          style={{lineHeight: 17, fontSize: 12, color: "#666", flex: 1, textAlign: "right"}}>
+          marginTop: 5
+        }}
+      >
+        <Text style={{ lineHeight: 17, fontSize: 12, color: "#666", flex: 1 }}>评论 {props.commentNum}</Text>
+        <Text style={{ lineHeight: 17, fontSize: 12, color: "#666", flex: 1, textAlign: "right" }}>
           点赞 {transformLikeNum(props.likesNum)}
         </Text>
       </View>
