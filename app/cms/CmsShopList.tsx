b5f668aac8c2812309e0d22cb824cfe7b6f03abd
何祥源 <xiangyuan.he@aihuishou.com>
2021-03-23 19:09:53 +0800

    方泡泡 cms

--- app/cms/CmsShopList.tsx
+++ app/cms/CmsShopList.tsx
@@ -9,7 +9,7 @@ import { Header } from "../components";
 import { FangHeApi } from "../services/api";
 import ToastRef from "../utils/Toast";
 import ToastGlobal from "../utils/Toast";
-import { useNavigation } from "@react-navigation/native"
+import { useNavigation } from "@react-navigation/native";
 
 const styles = StyleSheet.create({
   imgStyle: {
@@ -120,11 +120,10 @@ export const CmsShopItem = (props: CMSShopItemProps) => {
 };
 
 export const CmsShopList = () => {
-
   const [data, setData] = useState<Array<CMSShopItemProps>>([]);
 
   useEffect(() => {
-    FangHeApi.get("/shop/list")
+    FangHeApi.get("/shop/list", { isForAppStore: false })
       .then(value => {
         setData(value.data);
         ToastRef.show(value.code);
